f585100a0048c29210e92559a67427d213aa106a
GEONHWI SONG <geonhwi.song@student.tafesa.edu.au>
2024-03-26 16:33:44 +1030

    Added a feature_mortgage_calculator Added Mortgate calculator code

--- UniversalCalculator/Mortgage.xaml.cs
+++ UniversalCalculator/Mortgage.xaml.cs
@@ -26,5 +26,40 @@ namespace Calculator
 		{
 			this.InitializeComponent();
 		}
+
+		private void calculateButton_Click(object sender, RoutedEventArgs e)
+		{
+			{
+				double principal = double.Parse(principalTextBox.Text);
+				double annualInterestRate = double.Parse(yearlyinterestrateTextBox.Text);
+				int years = int.Parse(yearsTextBox.Text);
+				int months = int.Parse(monthTextBox.Text);
+				int totalMonths = years * 12 + months;
+
+				double monthlyInterestRate = CalcMonthlyInterestRate(annualInterestRate);
+				double monthlyPayment = CalcMonthlyRepayment(principal, annualInterestRate, totalMonths);
+
+				mothlyrepaymentTextBox.Text = monthlyPayment.ToString("C2");
+				monthlyinterestrateTextBox.Text = monthlyInterestRate.ToString("N4") + "%";
+			}
+		}
+		private double CalcMonthlyInterestRate(double annualInterestRate)
+		{
+			double monthlyInterestRate = (annualInterestRate / 100) / 12;
+			return monthlyInterestRate;
+		}
+		private double CalcMonthlyRepayment(double principal, double annualInterestRate, int totalMonths)
+		{
+			double monthlyInterestRate = (annualInterestRate / 100) / 12;
+			double monthlyPayment = (principal * monthlyInterestRate * Math.Pow((1 + monthlyInterestRate), totalMonths)) /
+												(Math.Pow((1 + monthlyInterestRate), totalMonths) - 1);
+
+			return monthlyPayment;
+		}
+
+		//private void exitButton_Click(object sender, RoutedEventArgs e)
+		//{
+		//	//Frame.Navigate(typeof(Menu.MainPage));
+		//}
 	}
 }
